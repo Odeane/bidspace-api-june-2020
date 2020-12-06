d1ec29c9ebd85c39b6754554c8253b5974438bf2
odeane_croney <odeane_croney@yahoo.com>
2020-12-07 00:13:25 +0100

    adding new imgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ free_listing_1 = Listing.create(
   price: '50',
   landlord_id: user.id
 )
-file = URI.open('https://www.quickanddirtytips.com/sites/default/files/images/12186/Parking-Lot-of-deas.jpg')
+file = URI.open('https://images.unsplash.com/photo-1600542431895-dfe1c68eb1c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80')
 free_listing_1.images.attach(io: file, filename: 'first.jpg')
-file = URI.open('https://beginnerspassiveincome.com/wp-content/uploads/2019/05/Rent-parking-spot-large-min.jpg')
+file = URI.open('https://images.unsplash.com/photo-1585360144219-236e7fc8e17b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
 free_listing_1.images.attach(io: file, filename: 'forth.jpg')
 
 free_listing_2 = Listing.create(
@@ -27,9 +27,9 @@ free_listing_2 = Listing.create(
   price: '80',
   landlord_id: user.id
 )
-file = URI.open('https://www.pymnts.com/wp-content/uploads/2018/03/parkingspot.jpg')
+file = URI.open('https://images.unsplash.com/photo-1580066620217-c9353ec0f476?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
 free_listing_2.images.attach(io: file, filename: 'second.jpg')
-file = URI.open('https://beginnerspassiveincome.com/wp-content/uploads/2019/05/Rent-parking-spot-large-min.jpg')
+file = URI.open('https://images.unsplash.com/photo-1580137331426-c28eb6be023b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1267&q=80')
 free_listing_2.images.attach(io: file, filename: 'forth.jpg')
 
 free_listing_3 = Listing.create(
@@ -41,7 +41,7 @@ free_listing_3 = Listing.create(
   price: '100',
   landlord_id: subscriber.id
 )
-file = URI.open('https://parkeasier.com/wp-content/uploads/2014/12/Homepage_Find_a_Spot_section2.jpg')
+file = URI.open('https://images.unsplash.com/photo-1526626607369-f89fe1ed77a9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')
 free_listing_3.images.attach(io: file, filename: 'third.jpg')
-file = URI.open('https://beginnerspassiveincome.com/wp-content/uploads/2019/05/Rent-parking-spot-large-min.jpg')
+file = URI.open('https://images.unsplash.com/photo-1587409201075-7ffa985e9d2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1363&q=80')
 free_listing_3.images.attach(io: file, filename: 'forth.jpg')
